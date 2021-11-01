2f62dd0d2b5dd4953f278e9a3e2c8de345cf36ee
khodosko <dkhodosko@gmail.com>
2021-10-31 19:01:19 -0700

    Added function1 module

---
+++ notebooks/project_functions1.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+import seaborn as sns #Understanding my variables
+from IPython.display import display
+import mplfinance as fplt
+
+def load_and_process(url_or_path_to_csv_file):
+	ddf = (pd.read_csv(url_or_path_to_csv_file, sep=",", header=0)
+	.assign(DailyReturn = lambda x: x['Close']
+	.pct_change())
+	.drop('OpenInt', 
+	axis=1))
+	
+	
+	ddf.dropna(inplace = True, axis = 0)
+	ddf['Date'] = pd.to_datetime(ddf['Date'])
+	ddf.index = ddf["Date"]
+	ddf = ddf.loc['1995-01-01':'2002-10-01']
+
+	return ddf
