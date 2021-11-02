68e85371c39ff0decf103ff4496a0e77c4822381
dimaubco <dmitry.kostyukov01@gmail.com>
2021-11-01 17:41:02 -0700

    Fixed method chaining in Analysis2 and project_functions2

--- notebooks/project_functions2.py
+++ notebooks/project_functions2.py
@@ -5,8 +5,9 @@ import matplotlib.pylab as plt
 import seaborn as sns #Understanding my variables
 
 def load_and_process(url_or_path_to_csv_file):
-    df = (pd.read_csv(url_or_path_to_csv_file, sep=",", header=0)
-        .drop(['OpenInt'], axis=1)    
-        df['Date'] = pd.to_datetime(df['Date'])
-        df = [df['Volume'].between(1, 1.0e+15)]
-        return df
+    df = (pd.read_csv(url_or_path_to_csv_file, sep=",", header=0).drop(['OpenInt'], axis=1))
+    df['Date'] = pd.to_datetime(df['Date'])
+    df = [df['Volume'].between(1, 1.0e+15)]
+
+          
+    return df
