d2ada354fa71412c213f14fa3da68d0911529f7d
pnuttydutty <pnuttydutty@gmail.com>
2021-11-24 10:06:00 -0800

    added dashboard and continued some function work in my notebook.

--- notebooks/project_functions3.py
+++ notebooks/project_functions3.py
@@ -7,11 +7,11 @@ plt.show()
 plt.rc("axes.spines", top=False, right=False)
 
 
-def load_and_process(url_or_path_to_csv_file, start, end):
+def random_pick(url_or_path_to_csv_file):
     
     '''
     load_and_process takes a random filename, chosen among the files of the given path.
-    It also indicates a start and end date denoted as 'YYYY-MM-DD', processes the dataset and returns a plot and a table.
+    
     '''
     
     n=0
@@ -23,8 +23,16 @@ def load_and_process(url_or_path_to_csv_file, start, end):
             rfile=os.path.join(root, name)
     print(rfile)
     
+
+def load_and_process(url_or_path_to_csv_file, start, end):
+    
+    '''
+    load_and_process takes a filename, chosen among the files of the given path.
+    It also indicates a start and end date denoted as 'YYYY-MM-DD', processes the dataset and returns a plot and a table.
+    '''
+
     # Method Chain 1 - load data
-    df = pd.read_csv(rfile, sep=",", header=0, names=["Date", "Open", "High", "Low", "Close", "Volume", "OpenInt"])
+    df = pd.read_csv(url_or_path_to_csv_file, sep=",", header=0, names=["Date", "Open", "High", "Low", "Close", "Volume", "OpenInt"])
     
     # Method Chain 2 - set time series index
     df['Date'] = df['Date'].apply(pd.to_datetime)
@@ -40,5 +48,5 @@ def load_and_process(url_or_path_to_csv_file, start, end):
     end_date = end
     df[(start_date <= df.index) & (df.index <= end_date)]['Close'].resample('M').mean().plot(kind ='bar', grid=False, figsize = (16,8))
     plt.title(f'Monthly Mean Close Price for Security between {start} and {end}')
-    df.head()
+    return df.head()
     
