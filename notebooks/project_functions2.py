2cb361d6d2eedef7a0c01d25b5b3c39685eba07c
dimaubco <dmitry.kostyukov01@gmail.com>
2021-11-27 15:05:52 -0800

    Added changes to method chaining (analysis2)

--- notebooks/project_functions2.py
+++ notebooks/project_functions2.py
@@ -5,9 +5,17 @@ import matplotlib.pylab as plt
 import seaborn as sns #Understanding my variables
 
 def load_and_process(url_or_path_to_csv_file):
-    df = (pd.read_csv(url_or_path_to_csv_file, sep=",", header=0).drop(['OpenInt'], axis=1))
+    df = (pd.read_csv(url_or_path_to_csv_file, sep=",", header=0)
+    .assign(
+            RSI = lambda x: 100 - (100/(1+((((x['Close'].diff()).clip(lower=0)).ewm(com=13, adjust=False).mean())/((-1*(x['Close'].diff()).clip(upper=0)).ewm(com=13, adjust=False).mean())))),
+            MACD = lambda x: (x['Close'].ewm(span=12, adjust=False).mean()) - (x['Close'].ewm(span=26, adjust=False).mean()),
+            Signal_line = x['MACD'].ewm(span=9, adjust=False).mean()
+    )
+    .drop(['OpenInt'], axis=1))
+    
     df['Date'] = pd.to_datetime(df['Date'])
     df = [df['Volume'].between(1, 1.0e+15)]
+    df.index = df["Date"]
 
           
     return df
